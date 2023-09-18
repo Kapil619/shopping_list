7128df3b792b35c1932e09ca3397bc4b3fa2cf5b
kapil619 <kapilbadokar321@gmail.com>
2023-09-18 14:03:45 +0530

    sending a post request to the backend

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import '../models/category.dart';
 
 class NewItem extends StatefulWidget {
@@ -21,11 +22,20 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(GroceryItem(
-          id: DateTime.now().toString(),
-          name: _enteredName,
-          quantity: _enteredQuantity,
-          category: _selectedCategory));
+      final url = Uri.https(
+          'shoppinglist619-default-rtdb.firebaseio.com', 'shopping-list.json');
+      http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(
+          {
+            'name ': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          },
+        ),
+      );
+      // Navigator.of(context).pop();
     }
   }
 
